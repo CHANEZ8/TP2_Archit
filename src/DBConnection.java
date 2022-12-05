7775c3c8cd91a3d7573029aa8d3f3ebf066bb20f
CHANEZ8 <96837955+CHANEZ8@users.noreply.github.com>
2022-12-05 19:23:22 +0100

    question 2

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -3,23 +3,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
+		private String BDD = "nomBD";
+		private String url = "jdbc:mysql://localhost:3306/" + BDD;
+		private String user = "root";
+		private String passwd = "";
+		private Connection conn;
+		public static DBConnection dBConnection;		
 	   
-		String BDD = "nomBD";
-		String url = "jdbc:mysql://localhost:3306/" + BDD;
-		String user = "root";
-		String passwd = "";
-	    private Connection conn;
+	
 
 	   
-	    public DBConnection() throws SQLException {
-			conn=DriverManager.getConnection(url, user,passwd);
+	    private DBConnection() {
+			
 		}
 
+		
+		public static DBConnection getDBConnection() {
+	    	if ( dBConnection == null ){
+	    		dBConnection = new DBConnection();
+	    	}
+	    	return dBConnection;
+	    }
 	    
-	    public Connection getConn() {
+	    public Connection getConn() throws SQLException {
+			conn=DriverManager.getConnection(url, user,passwd);
 			return conn;
 		}
 
+
+		
 
 		
 	
